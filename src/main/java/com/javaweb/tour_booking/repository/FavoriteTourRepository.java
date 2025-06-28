11efbb00567646cdd66637504e0a5ae25741ad40
Thien P. Dang <23521476@gm.uit.edu.vn>
2025-06-28 17:35:06 +0700

    fix(AccountService): cập nhật logic gửi email và cải thiện xử lý lỗi; thay đổi định dạng email mật khẩu fix(CustomerController): thay đổi điểm cuối getCustomerById để sử dụng phương thức GET fix(FavoriteTourServiceImpl): thêm kiểm tra tính duy nhất cho các tour du lịch yêu thích

--- src/main/java/com/javaweb/tour_booking/repository/FavoriteTourRepository.java
+++ src/main/java/com/javaweb/tour_booking/repository/FavoriteTourRepository.java
@@ -3,7 +3,9 @@ package com.javaweb.tour_booking.repository;
 import com.javaweb.tour_booking.entity.FavoriteTour;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface FavoriteTourRepository extends JpaRepository<FavoriteTour, Long> {
     List<FavoriteTour> findByUserId(Long userId);
+    Optional<FavoriteTour> findByUserIdAndTourRouteId(Long userId, Long tourRouteId);
 }
